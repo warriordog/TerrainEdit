8c91d003ec3a4622567885554ece54d8bc8edc96
warriordog <acomputerdog@gmail.com>
2013-10-17 14:45:59 -0400

    Add Cuboid and CuboidTable

---
+++ net/acomputerdog/TerrainEdit/cuboid/Cuboid.java
@@ -0,0 +1,80 @@
+package net.acomputerdog.TerrainEdit.cuboid;
+
+/**
+ * Represents a rectangular cuboid specified by 2 sets of coordinates.
+ */
+public class Cuboid {
+    protected int xPos1;
+    protected int yPos1;
+    protected int zPos1;
+    protected int xPos2;
+    protected int yPos2;
+    protected int zPos2;
+    protected boolean isSet;
+
+    public Cuboid(int xPos1, int yPos1, int zPos1, int xPos2, int yPos2, int zPos2, boolean isSet) {
+        this.xPos1 = xPos1;
+        this.yPos1 = yPos1;
+        this.zPos1 = zPos1;
+        this.xPos2 = xPos2;
+        this.yPos2 = yPos2;
+        this.zPos2 = zPos2;
+        this.isSet = isSet;
+    }
+
+    public int getXPos1() {
+        return xPos1;
+    }
+
+    public void setXPos1(int xPos1) {
+        this.xPos1 = xPos1;
+    }
+
+    public int getYPos1() {
+        return yPos1;
+    }
+
+    public void setYPos1(int yPos1) {
+        this.yPos1 = yPos1;
+    }
+
+    public int getZPos1() {
+        return zPos1;
+    }
+
+    public void setZPos1(int zPos1) {
+        this.zPos1 = zPos1;
+    }
+
+    public int getXPos2() {
+        return xPos2;
+    }
+
+    public void setXPos2(int xPos2) {
+        this.xPos2 = xPos2;
+    }
+
+    public int getYPos2() {
+        return yPos2;
+    }
+
+    public void setYPos2(int yPos2) {
+        this.yPos2 = yPos2;
+    }
+
+    public int getZPos2() {
+        return zPos2;
+    }
+
+    public void setZPos2(int zPos2) {
+        this.zPos2 = zPos2;
+    }
+
+    public void setIsSet(boolean isSet){
+        this.isSet = isSet;
+    }
+
+    public boolean getIsSet(){
+        return this.isSet;
+    }
+}
---
+++ net/acomputerdog/TerrainEdit/cuboid/CuboidTable.java
@@ -0,0 +1,37 @@
+package net.acomputerdog.TerrainEdit.cuboid;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A table mapping Cuboids to their players.
+ */
+public class CuboidTable {
+    private static Map<String, Cuboid> table = new HashMap<String, Cuboid>();
+
+    /**
+     * Gets the cuboid for a player, creating it if needed.
+     * @param player The player to create the cuboid for.
+     * @return Return the cuboid for the player.
+     */
+    public static Cuboid getCuboidForPlayer(String player){
+        Cuboid cuboid = table.get(player);
+        if(cuboid == null){
+            cuboid = new Cuboid(0, 0, 0, 0, 0, 0, false);
+            table.put(player, cuboid);
+        }
+        return cuboid;
+    }
+
+    public static boolean doesPlayerHaveCuboid(String player){
+        return table.containsKey(player);
+    }
+
+    public static void setCuboidForPlayer(String player, Cuboid cuboid){
+        table.put(player, cuboid);
+    }
+
+    public static Map<String, Cuboid> getTable(){
+        return table;
+    }
+}
