c8151c115c33e6beb0af6ae2dd2f85c63e48f369
warriordog <acomputerdog@gmail.com>
2013-10-23 16:01:16 -0400

    Update to new version of BL for Demo2 release

--- net/acomputerdog/TerrainEdit/main/ModTerrainEdit.java
+++ net/acomputerdog/TerrainEdit/main/ModTerrainEdit.java
@@ -66,7 +66,7 @@ public class ModTerrainEdit extends Mod {
         if(!Version.getMinecraftVersion().equals("1.6.4")){
             System.out.println("TerrainEdit - Incorrect Minecraft version, aborting launch!");
             return false;
-        }else if(!(Version.getGlobalVersion() == 0 && Version.getApiVersion() <= 7)){
+        }else if(!(Version.getGlobalVersion() == 0 && Version.getApiVersion() <= 8)){
             System.out.println("TerrainEdit - Incorrect BL version, bad things may happen!");
         }
         return true;
