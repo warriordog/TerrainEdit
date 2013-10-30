f6edf2ccf01a5846edef0a5d2949268c9e4b6f56
warriordog <acomputerdog@gmail.com>
2013-10-30 15:17:26 -0400

    Update TE to new BL version.  No internal changes besides version check.

--- net/acomputerdog/TerrainEdit/main/ModTerrainEdit.java
+++ net/acomputerdog/TerrainEdit/main/ModTerrainEdit.java
@@ -66,10 +66,10 @@ public class ModTerrainEdit extends Mod {
         if(!Version.getMinecraftVersion().equals("1.6.4")){
             System.out.println("TerrainEdit - Incorrect Minecraft version, aborting launch!");
             return false;
-        }else if(Version.getGlobalVersion() == 0 && Version.getApiVersion() >= 16){
+        }else if(Version.getGlobalVersion() == 1 && Version.getApiVersion() > 0){
             System.out.println("TerrainEdit - Unknown BL version, bad things may happen!");
             return true;
-        }else if(Version.getGlobalVersion() != 0 || Version.getApiVersion() <= 13){
+        }else if(Version.getGlobalVersion() != 1 || Version.getApiVersion() <= -1){
             System.out.println("TerrainEdit - Incompatible BL version, aborting launch!");
             return false;
         }else{
