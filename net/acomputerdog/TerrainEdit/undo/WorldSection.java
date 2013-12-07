2bbc1a56b5cc3343857376e25d72080a48ec1b59
warriordog <acomputerdog@gmail.com>
2013-12-07 08:37:11 -0500

    Fix all compile issues

--- net/acomputerdog/TerrainEdit/undo/WorldSection.java
+++ net/acomputerdog/TerrainEdit/undo/WorldSection.java
@@ -1,7 +1,9 @@
 package net.acomputerdog.TerrainEdit.undo;
 
+import net.acomputerdog.BlazeLoader.api.block.ApiBlock;
 import net.acomputerdog.BlazeLoader.api.block.ENotificationType;
 import net.acomputerdog.TerrainEdit.cuboid.Cuboid;
+import net.minecraft.block.Block;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -11,9 +13,9 @@ import net.minecraft.world.World;
  */
 public class WorldSection {
     private int x1, y1, z1, x2, y2, z2;
-    private int[] blockIDs;
+    private Block[] blocks;
     private int[] blockDATAs;
-    private NBTTagCompound tileEntities = new NBTTagCompound("tileEntities");
+    private NBTTagCompound tileEntities = new NBTTagCompound();
 
     public WorldSection(int x1, int y1, int z1, int x2, int y2, int z2, World world) {
         this.x1 = x1;
@@ -22,18 +24,19 @@ public class WorldSection {
         this.x2 = x2;
         this.y2 = y2;
         this.z2 = z2;
-        blockIDs = new int[(Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1) * (Math.abs(z2 - z1) + 1)];
+        blocks = new Block[(Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1) * (Math.abs(z2 - z1) + 1)];
         blockDATAs = new int[(Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1) * (Math.abs(z2 - z1) + 1)];
         int currIndex = 0;
         for(int currX = Math.min(x1, x2); currX <= Math.max(x2, x1); currX++){
             for(int currY = Math.min(y1, y2); currY <= Math.max(y2, y1); currY++){
                 for(int currZ = Math.min(z1, z2); currZ <= Math.max(z2, z1); currZ++){
-                    blockIDs[currIndex] = world.getBlockId(currX, currY, currZ);
+                    blocks[currIndex] = ApiBlock.getBlock(world, currX, currY, currZ);
                     blockDATAs[currIndex] = world.getBlockMetadata(currX, currY, currZ);
-                    if(world.blockHasTileEntity(currX, currY, currZ)){
-                        NBTTagCompound thisTile = new NBTTagCompound("tile");
-                        world.getBlockTileEntity(currX, currY, currZ).writeToNBT(thisTile);
-                        tileEntities.setCompoundTag(currX + "," + currY + "," + currZ, thisTile);
+                    TileEntity te = world.func_147438_o(currX, currY, currZ);
+                    if(te != null){
+                        NBTTagCompound thisTile = new NBTTagCompound();
+                        te.func_145841_b(thisTile);
+                        tileEntities.setTag(currX + "," + currY + "," + currZ, thisTile);
                     }
                     currIndex++;
                 }
@@ -50,9 +53,10 @@ public class WorldSection {
         for(int currX = Math.min(x1, x2); currX <= Math.max(x2, x1); currX++){
             for(int currY = Math.min(y1, y2); currY <= Math.max(y2, y1); currY++){
                 for(int currZ = Math.min(z1, z2); currZ <= Math.max(z2, z1); currZ++){
-                    world.setBlock(currX, currY, currZ, blockIDs[currIndex], blockDATAs[currIndex], ENotificationType.NOTIFY_CLIENTS.getType());
-                    if(world.blockHasTileEntity(currX, currY, currZ)){
-                        world.setBlockTileEntity(currX, currY, currZ, TileEntity.createAndLoadEntity(tileEntities.getCompoundTag(currX + "," + currY + "," + currZ)));
+                    ApiBlock.setBlock(world, currX, currY, currZ, blocks[currIndex], blockDATAs[currIndex], ENotificationType.NOTIFY_CLIENTS.getType());
+                    TileEntity te = world.func_147438_o(currX, currY, currZ);
+                    if(te != null){
+                        world.func_147455_a(currX, currY, currZ, TileEntity.func_145827_c(tileEntities.getCompoundTag(currX + "," + currY + "," + currZ)));
                     }
                     currIndex++;
                 }
